86eb79270124b18d36823ea6a445a8109be04d79
weizhi33 <a37705040@gmail.com>
2025-10-22 02:25:51 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 import streamlit as st
-st.title("我的第一個 Streamlit App")
-st.header("Hello, Geographers!(From Codespaces!)")
+import pandas as pd
+st.title("Streamlit  核心　Widgets")
+# 把widgets放到側邊攔 (sidebar)
+with st.sidebar:
+st.header("這裡是側邊攔")
+#  選擇框(Selectbox)
+option = st.selectbox(
+"你最喜歡的 GIS ?",
+("QGIS", "ArcGIS", "ENVI", "GRASS")
+)
+# 滑桿 (Slider)
+year = st.slider("選擇一個年份", 1990, 2030, 2024)
+# 2.在主業顯示 Widgets 的結果
+st.write(f"你選的軟體是 {option}")
+st.write(f"你選的年份是 {year}")
+# 按鈕 (Button)
+if st.button("點我顯示氣球"):
+st.balloons()
+# 檔案上傳 (File Uploader) - 地理系必備!
+uploaded_file = st.file_uploader(
+"上傳你的 Shapefile (.zip) 或 GeoTIFF (.tif) 或 GeoJSON (.json)",
+type=["zip", "tif", "json"]
+)
+if uploaded_file is not None:
+st.success(f"你上傳了: {uploaded_file.name} (大小: {uploaded_file.size} bytes)")
