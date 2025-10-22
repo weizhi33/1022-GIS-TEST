0b0e5129fa9843d918b369a57b980d6e3759fa9c
weizhi33 <a37705040@gmail.com>
2025-10-22 03:49:42 +0000

    modified:   page_home.py

--- page_home.py
+++ page_home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("陳威齊s1143056")
-st.header("陳威齊的地理資訊系統運用程式-Streamlit-Web-App　作業")
+st.header("陳威齊的地理資訊系統運用程式１-Streamlit-Web-App　作業")
 st.write("""
 大家好，我是陳威齊，這是我的作業。
 """)
