6bbb64129f5e67e41cadcf83a0753be67cda9a57
weizhi33 <a37705040@gmail.com>
2025-10-22 03:12:02 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
-# 1. o st.Page() ÿ¿¯
-# ÿst.Page() ¯×~ .py þ
+# 1.使用 st.Page() ÿ¿¯
+# 注意：st.Page() 會自動尋找.py 檔案
 # Emoji Wÿhttps://tw.piliapp.com/emoji/list/
 pages = [
-st.Page("page_home.py", title="\¿¿"),
-st.Page("page_map.py", title="×W½"),
-st.Page("page_about.py", title="sQ")
+st.Page("page_home.py", title="關於我"),
+st.Page("page_map.py", title="地圖"),
+st.Page("page_about.py", title="不確定")
 ]
-# 2. o st.navigation() ½ (Ït)
+# 2.使用 st.navigation() 建立導覽 (例如在側邊攔)
 with st.sidebar:
-    st.title("App ½")
-# st.navigation() Þóö¿¯
+    st.title("App 導覽")
+# st.navigation() 會回傳被選擇的葉面
 selected_page = st.navigation(pages)
-# 3. ¯ö¿¯
+#執行被選擇的葉面
 selected_page.run()
